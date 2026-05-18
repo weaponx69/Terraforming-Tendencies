d394ad0a8924c006bbbd40fc3b28319f5a3e7677
Brian Perry <perry.brian@gmail.com>
2026-05-18 07:25:36 -0400

    refactor: increase edge pan size and remove crash log file

--- Assets/Scripts/Player/CameraConfig.cs
+++ Assets/Scripts/Player/CameraConfig.cs
@@ -7,7 +7,7 @@ namespace GameDevTV.RTS.Player
     {
         [field: SerializeField] public bool EnableEdgePan { get; private set; } = true;
         [field: SerializeField] public float MousePanSpeed { get; private set; } = 5;
-        [field: SerializeField] public float EdgePanSize { get; private set; } = 50;
+        [field: SerializeField] public float EdgePanSize { get; private set; } = 150;
 
         [field: SerializeField] public float KeyboardPanSpeed { get; private set; } = 5;
 
