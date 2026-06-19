d24c10dcf199f9f818f9373165c37fc99381d123
Brian Perry <perry.brian@gmail.com>
2026-06-19 02:13:34 -0400

    feat: add AssetMover script to automate deletion of obsolete building assets on load

--- Assets/Editor/AssetMover.cs
+++ Assets/Editor/AssetMover.cs
@@ -1,1 +1,44 @@
-// DELETED - Old one-off script no longer needed.
+using UnityEditor;
+using System.IO;
+using UnityEngine;
+
+[InitializeOnLoad]
+public static class AssetMover
+{
+    static AssetMover()
+    {
+        string[] filesToDelete = new string[]
+        {
+            "Assets/Units/Buildings/SolarPanel/SolarPanel.asset",
+            "Assets/Units/Buildings/SolarPanel/SolarPanel.asset.meta",
+            "Assets/Units/Buildings/Command Post/Command Post.asset",
+            "Assets/Units/Buildings/Command Post/Command Post.asset.meta",
+            "Assets/Units/Buildings/Habitat/Habitat.asset",
+            "Assets/Units/Buildings/Habitat/Habitat.asset.meta",
+            "Assets/Units/Buildings/Oxygen Processor/Oxygen Processor.asset",
+            "Assets/Units/Buildings/Oxygen Processor/Oxygen Processor.asset.meta"
+        };
+
+        bool deletedAny = false;
+        foreach (string file in filesToDelete)
+        {
+            string fullPath = Path.Combine(Application.dataPath, "..", file);
+            if (File.Exists(fullPath))
+            {
+                try
+                {
+                    File.Delete(fullPath);
+                    deletedAny = true;
+                }
+                catch (System.Exception)
+                {
+                }
+            }
+        }
+
+        if (deletedAny)
+        {
+            AssetDatabase.Refresh();
+        }
+    }
+}
